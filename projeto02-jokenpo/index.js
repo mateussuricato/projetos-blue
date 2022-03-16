81230c157b499d01b52704cc52db483817515991
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-03-16 18:13:53 -0300

    jokenpo blue

--- projeto02-jokenpo/index.js
+++ projeto02-jokenpo/index.js
@@ -1,77 +1,91 @@
 const prompt = require("prompt-sync")();
 
-let continuar = "sim"
+let continuar = "sim";
 
-while (continuar == 'sim') {
+while (continuar == "sim") {
+  let humano = 0;
 
-let humano = 0
+  let computador = 0;
 
-let computador = 0
+  let empate = 0;
 
-let empate = 0
+  console.log("Vamos jogar pedra, papel e tesoura?\n");
 
-console.log("Vamos jogar pedra, papel e tesoura?\n");
+  let rodadas = +prompt("Quantas rodadas você quer jogar: ") + 1;
+  while (isNaN(rodadas)) {
+    console.log("Quantas rodadas você quer jogar?");
+    rodadas = +prompt("Digite um número: ") + 1;
+  }
 
-const rodadas = +prompt("Quantas rodadas você quer jogar: ") + 1;
-
-for (let i = 1; i < rodadas; i++) {
-  console.log(`Escolha um número referente a opção:
+  for (let i = 1; i < rodadas; i++) {
+    console.log(`\nEscolha um número referente a opção:
 [0] - Pedra
 [1] - Papel
 [2] - Tesoura\n`);
 
-  const lista = ["Pedra", "Papel", "Tesoura"];
+    const lista = ["Pedra", "Papel", "Tesoura"];
 
-  let escolha = +prompt("Escolha uma opção: ");
+    let escolha = +prompt(`Estamos na rodada ${i} escolha uma opção: `);
 
-  const escolha2 = Math.floor(Math.random() * 3);
+    const escolha2 = Math.floor(Math.random() * 3);
 
-  while (escolha > 2) {
-    escolha = prompt("\nDigite um dos números correspondente: ");
+    while (escolha > 2) {
+      escolha = prompt("Digite um dos números correspondente: ");
+    }
+
+    console.clear();
+
+    console.log(`\nEssa foi a ${i} rodada`);
+    console.log(`\nSua escolha foi ${lista[escolha]}\n`);
+    console.log(`A escolha do computador foi ${lista[escolha2]}\n`);
+
+    if (escolha == escolha2) {
+      console.log("Empatou");
+      empate = empate + 1;
+    } else if (escolha == 0 && escolha2 == 1) {
+      console.log("O computador ganhou");
+      computador = computador + 1;
+    } else if (escolha == 0 && escolha2 == 2) {
+      console.log("Você ganhou");
+      humano = humano + 1;
+    } else if (escolha == 1 && escolha2 == 0) {
+      console.log("Você ganhou");
+      humano = humano + 1;
+    } else if (escolha == 1 && escolha2 == 2) {
+      console.log("o computador ganhou");
+      computador = computador + 1;
+    } else if (escolha == 2 && escolha2 == 0) {
+      console.log("O computador ganhou");
+      computador = computador + 1;
+    } else {
+      console.log("Você ganhou");
+      humano = humano + 1;
+    }
   }
-  console.log(`Você está na ${i} rodada`)
-  console.log(`\nSua escolha foi ${lista[escolha]}\n`);
-  console.log(`A escolha do computador foi ${lista[escolha2]}\n`);
 
-  if (escolha == escolha2) {
-    console.log("Empatou");
-    empate = empate + 1
-  } else if (escolha == 0 && escolha2 == 1) {
-    console.log("O computador ganhou");
-    computador = computador + 1
-  } else if (escolha == 0 && escolha2 == 2) {
-    console.log("Você ganhou");
-    humano = humano + 1
-  } else if (escolha == 1 && escolha2 == 0) {
-    console.log("Você ganhou");
-    humano = humano + 1
-  } else if (escolha == 1 && escolha2 == 2) {
-    console.log("o computador ganhou");
-    computador = computador + 1
-  } else if (escolha == 2 && escolha2 == 0) {
-    console.log("O computador ganhou");
-    computador = computador + 1
-  } else {
-    console.log("Você ganhou");
-    humano = humano + 1
-  }
-}
-
-console.log(`O computador fez ${computador} pontos
+  console.log(`\nO computador fez ${computador} pontos
 Você fez ${humano} pontos
-E teve ${empate} empate(s)`)
+E teve ${empate} empate(s)`);
 
-if (computador > humano) {
-    console.log('O computador foi o grande campeão')
-} else if (computador == humano){
-    console.log('As partidas acabaram empatadas')
-} else {
-    console.log('voce foi o grande campeão')
-}
-continuar = prompt('Digite [sim] para jogar novamente ou [nao] para sair: ').toLowerCase()
+  if (computador > humano) {
+    console.log("O COMPUTADOR foi o grande campeão");
+  } else if (computador == humano) {
+    console.log("As partidas acabaram EMPATADAS");
+  } else {
+    console.log("VOCÊ foi o grande campeão");
+  }
 
-while (continuar != 'sim' && continuar != 'nao') {
-    continuar = prompt('Digite [sim] para jogar novamente ou [nao] para sair: ').toLowerCase()
-}
-}
+  console.log();
 
+  continuar = prompt(
+    "Digite [sim] para jogar novamente ou [nao] para sair: "
+  ).toLowerCase();
+
+  console.clear();
+
+  while (continuar != "sim" && continuar != "nao") {
+    continuar = prompt(
+      "Digite [sim] para jogar novamente ou [nao] para sair: "
+    ).toLowerCase();
+  }
+}
